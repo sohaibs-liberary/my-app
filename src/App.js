85e98c53060849944bc8cdb9d0c030267ed8d39e
Sohaib-alt <muhammadsohaibshakeel18@gmail.com>
2025-07-17 19:18:22 +0500

    solve error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import Navbar from './components/navbar'
 
 function App() {
 
-  const [count , setcount ] = useState(0)
-  return (
-    <>
+  const [count, setcount] = useState(0)
+  const [first, setfirst] = useState(0)
+  const [color, setcolor] = useState(0)
+  
+  useEffect(() => {
 
-    <div>the count is {count}</div>
-    <button  onClick={()=> {setcount(count+1)}} >this is update</button>
+    alert('color was change')
+    setcolor(color +1)
+  }, [count])
+    
+  return (
+
+    <>
+      <Navbar color={"navy" + "blue" + color} />
+
+      <div>the count is {count}</div>
+      <button onClick={() => { setcount(count + 1) }} >this is update</button>
     </>
   )
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,81 @@
+import Reactl, {useEffect,useState}  from 'react'
+
+function Navbar({color}) {
+
+
+    // case 1: run on every render
+      useEffect(() => {
+        alert('hye!  Is was run on every render')
+      })
+    
+      //case 2: run only on first render
+      useEffect(() => {
+    
+    
+        alert('hye! thuis is my page , this is a firdt render')
+    
+    
+      }, [])
+      //case 3 : run only when certain value change
+    
+      useEffect(() => {
+    
+        alert('this is run when color is change')
+    
+      }, [color])
+    
+      useEffect(() => {
+    
+    
+        alert('this is new count')
+        setcolor(color + 1)
+    
+    
+      }, [count])
+
+
+    
+  return (
+    <div>
+      this is a  navbar color is {color} heheheheheehehe..............
+    </div>
+  )
+}
+
+export default Navbar;
+
+
+
+/*import Reactl, {useEffect,useState}  from 'react'
+
+function Navbar({ color }) {
+  const [count, setCount] = useState(0);
+  const [currentColor, setColor] = useState(color);
+
+  useEffect(() => {
+    alert('Hey! This runs on every render');
+  });
+
+  useEffect(() => {
+    alert('This runs only on first render');
+  }, []);
+
+  useEffect(() => {
+    alert('This runs when color prop changes');
+  }, [color]);
+
+  useEffect(() => {
+    alert('This runs when count changes');
+    setColor(currentColor + 1);
+  }, [count]);
+
+  return (
+    <div>
+      This is a navbar. Color is {currentColor}. Count is {count}.
+    </div>
+  );
+}
+
+export default Navbar
+
+*/
